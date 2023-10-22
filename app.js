e492dbcc224af63bb6ebc575ed6d58e8f6984ea4
Ahmed Karim <muhammadahmedkarimnagda@gmail.com>
2023-10-23 00:43:38 +0500

    add quiz box and more

---
+++ app.js
@@ -0,0 +1,50 @@
+var signInEmail = document.getElementById("signInEmail")
+var signInPassword = document.getElementById("signInPassword")
+
+
+function signIn(){
+if(signInEmail.value === "smit" && signInPassword.value === "1234"){
+    location.href = "quiz.html"
+}else{
+    alert("invalid password or email")
+}
+}
+
+
+var html = [ 
+    {
+        question: 'What does HTML stand for?',
+        option1: 'Hyperlinks and Text Markup Language',
+        option2: 'Hypertext Markup Language',
+        option3: 'Home Tool Markup Language',
+        correctOption: "Hypertext Markup Language"
+    },
+    {
+        question: 'Who is making the Web standards?',
+        option1: 'Google',
+        option2: 'The World Wide Web Consortium',
+        option3: 'Microsoft',
+        correctOption: "The World Wide Web Consortium"
+    },
+    {
+        question: 'Choose the correct HTML element for the largest heading:',
+        option1: '<heading>',
+        option2: '<h6>',
+        option3: '<h1>',
+        correctOption: "<h1>"
+    },
+    {
+        question: 'What is the correct HTML element for inserting a line break?',
+        option1: '<linebreak>',
+        option2: '<br>',
+        option3: '<break>',
+        correctOption: "<br>"
+    },
+    {
+        question: 'What is the correct HTML for adding a background color?',
+        option1: '<body bg="yellow">',
+        option2: '<background>yellow</background>',
+        option3: '<body style="background-color:yellow;">',
+        correctOption: '<body style="background-color:yellow;">'
+    },
+];
